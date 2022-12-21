af9e4ffa14e60ec1cb6f3ce7611ee002356294e5
neon_arch <mustafadhuleb53@gmail.com>
2022-12-21 19:20:35 +0300

    update scripts

--- usr-scripts/article-viewer-convertor.sh
+++ usr-scripts/article-viewer-convertor.sh
@@ -3,34 +3,35 @@
 # Please fail on hidden bugs
 set -euo pipefail
 
-zenity  --warning --text="DISCLAMER!!! \n Use the article for personal use only.\n Any illegal Activities shall not be held responsible by the developer" --ellipsize
+zenity --warning --text="DISCLAMER!!! \n Use the article for personal use only.\n Any illegal Activities shall not be held responsible by the developer" --ellipsize
 
-cmd=`zenity --entry --text="enter any article url to download and convert for later viewing: " --width=1000`
+cmd=$(zenity --entry --text="enter any article url to download and convert for later viewing: " --width=1000)
 
-mkdir_on_fail()
-{
-    cd ~
-    mkdir -p rest/articles
-    cd articles
+mkdir_on_fail() {
+	cd ~
+	mkdir -p rest/articles
+	cd articles
 }
 
-fail_err()
-{
-    echo "ERROR: Failed to mkdir."
-    exit
+fail_err() {
+	echo "ERROR: Failed to mkdir."
+	exit
 }
 
 case $cmd in
-    "")
-	exit;;
-    *)
-	cd ~/rest/articles || mkdir_on_fail || fail_err 
+"")
+	exit
+	;;
+*)
+	cd ~/rest/articles || mkdir_on_fail || fail_err
 	aria2c -Umozilla/5.0 -x 10 -c $cmd
 	ls | zenity --text="all files in article directory" --list --column="files" &
-	Name=`zenity --entry --text="enter unique name for your article: " --width=100`
+	Name=$(zenity --entry --text="enter unique name for your article: " --width=100)
 	pandoc *.html -o "${Name}.docx"
 	pandoc *.php -o "${Name}.docx"
 	rm *.html
 	rm *.php
 	killall zenity
+	dunstify "Finished downloading!!" -u "normal"
+	;;
 esac
--- usr-scripts/battery-notifier.sh
+++ usr-scripts/battery-notifier.sh
@@ -3,21 +3,17 @@
 # Please fail on hidden bugs
 set -euo pipefail
 
-if [[ `acpi | grep 'Battery'` == '' ]]
-then
-    exit
+if [[ $(acpi | grep 'Battery') == '' ]]; then
+	exit
 fi
 
-while :
-do
-    N=`acpi | cut -d ' ' -f4 | cut -d ',' -f1 | cut -d '%' -f1`
-    M=`acpi | cut -d ' ' -f3 | cut -d ',' -f1`
-    if [[ $N -le "30" && $M != "Charging" ]]
-    then
-        zenity --info --text="battery running low \n please plug in your charger" --icon-name="bell" --ellipsize
-    elif [[ $N -ge "85" && $M == "Charging" ]]
-    then
-        zenity --info --text="battery is full \n please plug out your charger" --icon-name="bell" --ellipsize
-    fi
-    sleep 60
+while :; do
+	N=$(acpi | cut -d ' ' -f4 | cut -d ',' -f1 | cut -d '%' -f1)
+	M=$(acpi | cut -d ' ' -f3 | cut -d ',' -f1)
+	if [[ $N -le "30" && $M != "Charging" ]]; then
+		dunstify "please plug in your charger" -u "critical"
+	elif [[ $N -ge "85" && $M == "Charging" ]]; then
+		dunstify "please plug out your charger" -u "low"
+	fi
+	sleep 60
 done
